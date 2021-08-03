5aa71670eac83811ebb8847884683f0f47e92741
frank3stein <martinov.emil@gmail.com>
2021-08-03 22:02:51 +0200

    Added Header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 // In order to make the component generic, we create a react component separetely using the mount function
 // import { mount } from 'marketing/Marketing'
 
 // console.log(mount)
-export default () => <MarketingApp />;
+export default () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <MarketingApp />
+    </div>
+  </BrowserRouter>
+);
