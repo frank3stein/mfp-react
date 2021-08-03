d78890aad8ab3a884d587155481f697461007286
frank3stein <martinov.emil@gmail.com>
2021-08-03 14:40:38 +0200

    Container update for github actions

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,12 @@
-import { mount } from 'marketing/Marketing'
-import React, {useRef, useEffect} from 'react'
+import { mount } from "marketing/Marketing";
+import React, { useRef, useEffect } from "react";
 
 export default () => {
-    const ref = useRef(null)
+  const ref = useRef(null);
 
-    useEffect(()=> {
-        mount(ref.current)
-    })
+  useEffect(() => {
+    mount(ref.current);
+  });
 
-    return <div ref={ref} />
-}
+  return <div ref={ref} />;
+};
