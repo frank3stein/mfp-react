083003dbe5e56626baa3296a6f5aab9d0172fad0
frank3stein <martinov.emil@gmail.com>
2021-08-05 22:23:07 +0200

    Class Name prefix implemented to prevent the clash

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+
+// In this way we add another level of unique prefix to css so they wont clash when they are build.
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 // In order to make the component generic, we create a react component separetely using the mount function
 // import { mount } from 'marketing/Marketing'
 
 // console.log(mount)
-export default () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
-  </BrowserRouter>
-);
+export default () => {
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
