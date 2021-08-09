5ef6443cff32488a88ff356781cdbc80f87eaa33
frank3stein <martinov.emil@gmail.com>
2021-08-09 13:20:28 +0200

    Container navigation added. Need to connect to marketing now

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,24 @@
 import { mount } from "marketing/Marketing";
 import React, { useRef, useEffect } from "react";
+// we need the history object to update the it manually
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
+  // in order to communicate with the marketing module we pass in an object
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      // location object has a pathname property, we will rename it while destructuring
+      onNavigate: ({ pathname: nextPathname }) => {
+        console.log(
+          "The container noticed Navigation in Marketing",
+          nextPathname
+        );
+        history.push(nextPathname);
+      },
+    });
   });
 
   return <div ref={ref} />;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  // when history changes it will call onNavigate
+  history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
