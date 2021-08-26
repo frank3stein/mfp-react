5767bdb76ceae6845ba02608aec3773affa3b979
frank3stein <martinov.emil@gmail.com>
2021-08-26 18:51:03 +0200

    Forcing dashboard and marketing redeploy

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -1,20 +1,20 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const packageJson = require('../package.json');
-const commonConfig = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const commonConfig = require("./webpack.common");
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/dashboard/latest/',
+    filename: "[name].[contenthash].js",
+    publicPath: "/dashboard/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'dashboard',
-      filename: 'remoteEntry.js',
+      name: "dashboard",
+      filename: "remoteEntry.js",
       exposes: {
-        './DashboardApp': './src/bootstrap',
+        "./DashboardApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
     }),
